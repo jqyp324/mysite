22cc57b2f27d21a4fe3104a239c99f4bf9a31c89
yupeng112527 <yupeng112527@pwrd.com>
2018-04-24 14:05:33 +0800

    测试用例写法

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Question, Choice
+from django.test import TestCase
+from django.utils import timezone
+import datetime
+
 
 # Create your tests here.
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
