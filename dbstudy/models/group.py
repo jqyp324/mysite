4008e05b9976ba6ce135350a9b5bbb1b56818f21
yupeng112527 <yupeng112527@pwrd.com>
2018-04-28 18:06:08 +0800

    拆分模块

--- dbstudy/models/group.py
+++ dbstudy/models/group.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-class Person(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
-
-
 class Group(models.Model):
     name = models.CharField(max_length=26)
     members = models.ManyToManyField('Person')
