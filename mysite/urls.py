489dedb581474de0b45738ebd26e408fabdd40e3
jqyp324 <jqyp324@gmail.com>
2018-05-10 00:48:39 +0800

    不知道干了啥

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from polls import views
 
 urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
