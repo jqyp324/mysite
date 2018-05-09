0faad18fa2cfa32de43706e9213ebcce88f868a7
yupeng112527 <yupeng112527@pwrd.com>
2018-05-09 18:50:26 +0800

    自定义标签

--- polls/templatetags/polls_mycustom.py
+++ polls/templatetags/polls_mycustom.py
@@ -1,4 +1,5 @@
 from django import template
+import datetime
 
 register = template.Library()
 
@@ -9,3 +10,10 @@ def change_name(value, index):
         return value.replace(value[index], 'yupeng')
     else:
         return value
+
+
+@register.simple_tag(name='nowtime')
+def current_time():
+    return str(datetime.datetime.now())
+
+# print(type(str(datetime.datetime.now())))
